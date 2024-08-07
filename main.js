64e9fdc0c054410d818fc973652ef77d7259db9e
Mayra Méndez <111815136+mayra1703@users.noreply.github.com>
2024-08-07 01:34:27 -0700

    Portafolio completo

--- main.js
+++ main.js
@@ -1,1 +1,10 @@
-console.log("Hola Mundo desde consola!");
+var app = document.getElementById('app');
+
+var typewriter = new Typewriter(app, {
+    loop: true
+});
+
+typewriter.typeString('Desarrolladora Frontend Jr.')
+    .pauseFor(2500)
+    .deleteAll()
+    .start();
