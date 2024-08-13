0d374477e3850b1474d031c1d8841ac4b0046f9d
Mayra Méndez <111815136+mayra1703@users.noreply.github.com>
2024-08-12 19:17:43 -0700

    add download cv

--- main.js
+++ main.js
@@ -8,3 +8,17 @@ typewriter.typeString('Desarrolladora Frontend Jr.')
     .pauseFor(2500)
     .deleteAll()
     .start();
+
+let abierto = false;
+let ventana;
+
+document.getElementById('abrir-pdf').addEventListener('click', function() {
+        if (!abierto) {
+            ventana = window.open("Curriculum-Mayra-Méndez.pdf", '_blank');
+            abierto = true;
+        }
+
+        else {
+            ventana.focus();
+        }
+});
